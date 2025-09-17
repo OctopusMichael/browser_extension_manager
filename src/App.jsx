30fd3e4e87a4976109a2b2e203dcf5edfd293be8
Michael <diazcampomichael@gmail.com>
2025-09-17 14:49:35 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
+import { useEffect, useState } from "react";
 import Card from "./components/Card";
 import Header from "./components/Header";
+import jsonData from "./json/data.json";
 
 function App() {
+  const [data, setData] = useState(() => {
+    const saved = localStorage.getItem("data");
+    return saved ? JSON.parse(saved) : jsonData;
+  });
+
+  const [filter, setFilter] = useState("All");
+
+  useEffect(() => {
+    localStorage.setItem("data", JSON.stringify(data));
+  }, [data]);
+
+  const removeData = (name) => {
+    const updatedData = data.filter((item) => item.name !== name);
+    setData(updatedData);
+  };
+
+  const restoreAll = () => {
+    setData(jsonData);
+  }
+
+  const toggleData = (name) => {
+    const updatedData = data.map((item) =>
+      item.name === name ? { ...item, isActive: !item.isActive } : item
+    );
+    setData(updatedData);
+  };
+
+  const filteredData = data.filter((data) => {
+    if (filter === "active") return data.isActive;
+    if (filter === "inactive") return !data.isActive;
+    return true; // "all"
+  });
+
   return (
     <>
       <div className="min-h-screen bg-gradient-to-b from-[#EBF2FC] to-[#EEF8F9] dark:from-[#040918] dark:to-[#091540] text-white">
@@ -14,25 +49,42 @@ function App() {
             <section className="w-full flex flex-col md:flex-row justify-between gap-5  items-center mb-8 dark:text-neutral-0 text-black">
               <h1 className="text-4xl font-bold">Extencions List</h1>
               <div className="flex justify-between items-center gap-2">
-                <button className="rounded-3xl md:px-5 md:py-3 p-5  bg-neutral-0  text-2xl  dark:bg-neutral-800 hover:bg-red-400">All</button>
-                <button className="rounded-3xl md:px-5 md:py-3 p-5  bg-neutral-0  text-2xl dark:bg-neutral-800 hover:bg-red-400">Active</button>
-                <button className="rounded-3xl md:px-5 md:py-3 p-5   bg-neutral-0  text-2xl  dark:bg-neutral-800 hover:bg-red-400">Inactive</button>
+                <button
+                  onClick={() => setFilter("all")}
+                  className="rounded-3xl md:px-5 md:py-3 p-5  bg-neutral-0  text-2xl  dark:bg-neutral-800 hover:bg-red-400"
+                >
+                  All
+                </button>
+                <button
+                  onClick={() => setFilter("active")}
+                  className="rounded-3xl md:px-5 md:py-3 p-5  bg-neutral-0  text-2xl dark:bg-neutral-800 hover:bg-red-400"
+                >
+                  Active
+                </button>
+                <button
+                  onClick={() => setFilter("inactive")}
+                  className="rounded-3xl md:px-5 md:py-3 p-5   bg-neutral-0  text-2xl  dark:bg-neutral-800 hover:bg-red-400"
+                >
+                  Inactive
+                </button>
               </div>
             </section>
             <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-              <Card />
+              {filteredData.map((data, index) => (
+                <Card
+                  key={index}
+                  data={data}
+                  toggleData={toggleData}
+                  removeData={removeData}
+                />
+              ))}
             </section>
+            <button
+              onClick={restoreAll}
+              className="mt-10 rounded-3xl md:px-5 md:py-3 p-5 bg-gradient-to-b from-[#EBF2FC] to-[#EEF8F9] dark:from-[#040918] dark:to-[#091540] text-white dark:text-neutral-0 text-2xl font-bold border-2 border-neutral-0 hover:bg-red-400"
+            >
+              Restore All
+            </button>
           </main>
         </div>
       </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,31 +1,31 @@
-import React, { useState } from "react";
 
-const Card = () => {
-  const [enabled, setEnabled] = useState(false);
+const Card = ({ data, toggleData, removeData }) => {
+
   return (
     <div className="bg-neutral-0 dark:bg-neutral-800 rounded-2xl p-5 max-w-md flex flex-col h-[250px] justify-between shadow-2xl">
       <div className="flex items-start  justify-between gap-4">
-        <img src="images/logo-speed-boost.svg" alt="icon-console-card" />
+        <img src={`${data.logo}`} alt="icon-console-card" />
         <div className="flex flex-col gap-2 text-neutral-900 dark:text-neutral-0 ">
-          <h1 className="text-2xl font-bold">SpeedBoost</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam,
-            repudiandae.
-          </p>
+          <h1 className="text-2xl font-bold">{data.name}</h1>
+          <p>{data.description}</p>
         </div>
       </div>
       <div className="flex justify-between items-center  ">
-        <button className="text-neutral-900 dark:text-neutral-0 border-1 rounded-3xl px-3 py-2 font-bold">Remove</button>
         <button
-          onClick={() => setEnabled(!enabled)}
+        onClick={() => removeData(data.name)}
+        className="text-neutral-900 dark:text-neutral-0 border-1 rounded-3xl px-3 py-2 font-bold">
+          Remove
+        </button>
+        <button
+           onClick={() => toggleData(data.name)}
           className={`relative inline-flex h-6 w-11 items-center rounded-full transition-colors ${
-            enabled ? "bg-red-500 dark:bg-red-500"  : "bg-neutral-300 dark:bg-neutral-600"
+            data.isActive
+              ? "bg-red-500 dark:bg-red-500"
+              : "bg-neutral-300 dark:bg-neutral-600"
           }`}
         >
           <span
-            className={`inline-block h-4 w-4 transform rounded-full bg-neutral-0 dark:bg-neutral-0 transition-transform ${
-              enabled ? "translate-x-6" : "translate-x-1"
-            }`}
+            className={`inline-block h-4 w-4 transform rounded-full bg-neutral-0 dark:bg-neutral-0 transition-transform ${ data.isActive ? "translate-x-6" : "translate-x-1"}`}
           />
         </button>
       </div>
